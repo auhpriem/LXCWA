b5d13638823f9aa2755d3a5de71648723160a725
auhpriem <aliaksandr.hlushko@LeverX.local>
2019-03-18 17:47:51 +0300

    HW7

---
+++ HW7/hw7_requests/controller/People.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/v2/ODataModel"
+], function (Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.walkthrough.controller.People", {
+
+		onInit : function () {
+			//
+		}
+
+	});
+});
